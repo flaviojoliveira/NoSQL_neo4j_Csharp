70d5049638086cf4ea647a66e2ecb88818dd6005
Andreson Gusmão Manzoli Silva <62163882+andreson-gms@users.noreply.github.com>
2022-06-08 18:52:44 -0300

    mit

--- NoSQL_CSharp_App/NoSQL_CSharp_App/App.cs
+++ NoSQL_CSharp_App/NoSQL_CSharp_App/App.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Neo4j.Driver;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,7 +17,55 @@ namespace NoSQL_CSharp_App
         {
             InitializeComponent();
         }
+        public class HelloWorldExample : IDisposable
+        {
+            private bool _disposed = false;
+            private readonly IDriver _driver;
 
+            ~HelloWorldExample() => Dispose(false);
+
+            public HelloWorldExample(string uri, string user, string password)
+            {
+                _driver = GraphDatabase.Driver(uri, AuthTokens.Basic(user, password));
+            }
+
+            public void PrintGreeting(string message)
+            {
+                using (var session = _driver.AsyncSession())
+                {
+                    var greeting = session.WriteTransactionAsync(tx =>
+                    {
+                        var result = tx.RunAsync("CREATE (a:Greeting) " +
+                                            "SET a.message = $message " +
+                                            "RETURN a.message + ', from node ' + id(a)",
+                            new { message });
+                        return result;
+                    });
+                    Console.WriteLine(greeting);
+                }
+            }
+
+            public void Dispose()
+            {
+                Dispose(true);
+                GC.SuppressFinalize(this);
+            }
+
+            protected virtual void Dispose(bool disposing)
+            {
+                if (_disposed)
+                    return;
+
+                if (disposing)
+                {
+                    _driver?.Dispose();
+                }
+
+                _disposed = true;
+            }
+
+            
+        }
         private void limpaFormulario()
         {
             tb_Nome.Clear();
@@ -46,6 +95,10 @@ namespace NoSQL_CSharp_App
             string email = tb_Email.Text;
 
             MessageBox.Show($"Nome: {nome} Idade: {idade}\nEmail: {email}");
+            using (var greeter = new HelloWorldExample("bolt://localhost:7687", "neo4j", "password"))
+            {
+                greeter.PrintGreeting($"Nome: {nome} Idade: {idade}\nEmail: {email}");
+            }
 
             limpaFormulario();
             tb_Nome.Focus();
