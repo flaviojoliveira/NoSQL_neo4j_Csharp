b9ff33183774773c6f36fa4315abd576bb8988ae
Andreson Gusmão Manzoli Silva <62163882+andreson-gms@users.noreply.github.com>
2022-06-08 15:53:47 -0300

    commit

---
+++ NoSQL_CSharp_App/NoSQL_CSharp_App/App.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NoSQL_CSharp_App
+{
+    public partial class App : Form
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        private void lb_Email_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lb_Nome_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lb_Idade_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
